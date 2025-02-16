f558fd2f96d2cc68ad952e609b8c56e84ef2f8e8
Victor Wong <victor_wong444@hotmail.com>
2025-02-15 21:21:14 -0800

    digital ocean database

--- config/config.js
+++ config/config.js
@@ -1,9 +1,20 @@
+require('dotenv').config();
+
+const dbPassword = process.env.DB_PASSWORD;
+const sslMode = process.env.SSL_MODE || 'DISABLED';  // Default to 'DISABLED' if not set
+
+// Configure SSL based on the SSL_MODE value from .env
+const sslConfig = sslMode === 'REQUIRED' 
+  ? { rejectUnauthorized: false } 
+  : false;  // Disable SSL if SSL_MODE is not 'REQUIRED'
+
 module.exports = {
-    dbConfig: {
-      host: "localhost",
-      user: "root", // Update as needed
-      password: "159753Vw!", // Update as needed
-      database: "lab5",
-    },
-  };
-  
+  dbConfig: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: dbPassword,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: sslConfig,  // Apply the SSL configuration
+  },
+};
